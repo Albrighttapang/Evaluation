2eecac0c1df6dbc47f7ac02f8badf21a201284d5
Albrighttapang <tapangalbright1@gmail.com>
2025-02-25 12:02:42 +0100

    5 numbers stored in array and displayed in ascending order

--- 0xc_task/user_data.c
+++ 0xc_task/user_data.c
@@ -1,21 +1,34 @@
 #include<stdio.h>
 int main(){
 char name[10];
-int a,b,c,d,e, age;
+int age;
+int i,temp,array[5];
 printf("Please enter your name\n");
 scanf("%s", name);
 printf("Please enter your age\n");
 scanf("%d", &age);
 printf("Hello, %s! You are %d years old.\n",name,age);
 printf("Please enter first  number\n");
-scanf("%d", &a);
+scanf("%d", &array[0]);
 printf("Please enter second number\n");
-scanf("%d", &b);
+scanf("%d", &array[1]);
 printf("Please enter third number\n");
-scanf("%d",&c);
+scanf("%d",&array[2]);
 printf("Please enter fourth number\n");
-scanf("%d", &d);
+scanf("%d", &array[3]);
 printf("Please enter fifth number\n");
-scanf("%d", &e);
+scanf("%d", &array[4]);
+for(i=0;i<5;i++){
+for(int j=0;j<5;j++){
+if(array[j]>array[j+1]){
+temp=array[j];
+array[j]=array[j+1];
+array[j+1]=temp;}
+}
+}
+printf("The numbers in ascending order are;\n");
+for(i=0;i<5;i++){
+printf("%d, ",array[i]);
+}
 return 0; }
 
