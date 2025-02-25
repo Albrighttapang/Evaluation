8ca7d36b47d4bfd04ecf22f40ebbfe5f540f6cb3
Albrighttapang <tapangalbright1@gmail.com>
2025-02-25 11:46:16 +0100

    Program receives name, age and number

---
+++ 0xc_task/user_data.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+char name[10];
+int a,b,c,d,e, age;
+printf("Please enter your name\n");
+scanf("%s", name);
+printf("Please enter your age\n");
+scanf("%d", &age);
+printf("Hello, %s! You are %d years old.\n",name,age);
+printf("Please enter first  number\n");
+scanf("%d", &a);
+printf("Please enter second number\n");
+scanf("%d", &b);
+printf("Please enter third number\n");
+scanf("%d",&c);
+printf("Please enter fourth number\n");
+scanf("%d", &d);
+printf("Please enter fifth number\n");
+scanf("%d", &e);
+return 0; }
+
